08752260b08802b50c9fdaaaa5d658eb24ca6007
MartinSchlegel <ms2152@cam.ac.uk>
2016-12-08 15:08:11 +0000

    initial commit of Cam branch

---
+++ app/src/main/java/com/example/eduardo/gshell/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.eduardo.gshell;
+
+import com.jcraft.jsch.*;
+import java.util.Properties;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.util.Log;
+import java.io.File;
+import java.util.List;
+import java.io.ByteArrayOutputStream;
+import android.os.AsyncTask;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public String output;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        // END TEST Server
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), HostFormActivity.class));
+            }
+        });
+
+        //--------
+        //listing files in directory to pass onto the ListAdapter Method
+        final String[] fileArray;
+        final File contextDir = getApplicationContext().getFilesDir();
+        File dataFilesDir = new File(contextDir.getAbsolutePath() + "/dataFiles");
+        //create dir to store dataFiles, in case it does not exist
+        try {
+            //make the new dir: dataFiles
+            dataFilesDir.mkdir();
+            //create a list of files in the dataFiles dir:
+
+        }
+        catch(Exception e){}
+
+        File[] lsDataFilesDir = dataFilesDir.listFiles();
+        fileArray = new String[lsDataFilesDir.length];
+        Integer[] imageId = new Integer[lsDataFilesDir.length];
+        for (int i = 0; i < lsDataFilesDir.length; ++i){
+            fileArray[i] = lsDataFilesDir[i].getName();
+            imageId[i] = R.drawable.shop;
+            //new File(contextDir.getAbsolutePath() + "/dataFiles"+"/"+fileArray[i]).delete();
+            Log.d("File", fileArray[i]);
+        }
+
+
+        //Log.d("File", "hi");
+
+        //---need to save files in new directory - need to change Server.java accordingly
+
+
+        ListView gridView = (ListView) findViewById(R.id.server_list);
+        final ListAdapter adapter = new ListAdapter(getApplicationContext(), fileArray, imageId);
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            /**
+             * Callback method to be invoked when an item in this AdapterView has
+             * been clicked.
+             * <p/>
+             * Implementers can call getItemAtPosition(position) if they need
+             * to access the data associated with the selected item.
+             *
+             * @param parent   The AdapterView where the click happened.
+             * @param view     The view within the AdapterView that was clicked (this
+             *                 will be a view provided by the adapter)
+             * @param position The position of the view in the adapter.
+             * @param id       The row id of the item that was clicked.
+             */
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Server s = Server.load(contextDir.getAbsolutePath() + "/dataFiles" + "/"+ adapter.labelId[position]);
+                Intent intent = new Intent(getApplicationContext(), qstat_activity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("SERVER", s);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/eduardo/gshell/Server.java
@@ -0,0 +1,62 @@
+package com.example.eduardo.gshell; /**
+ * Created by eduardo on 24/11/16.
+ */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import android.content.Context;
+import java.io.FileInputStream;
+import android.util.Log;
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+
+public class Server implements Serializable{
+    private static final long serialVersionUID=9023849032938493028L;
+    public String name;
+    public String user_name;
+    public String passwd;
+    public String hostname;
+    public int port = 22;
+
+    public Server(String name, String user_name, String passwd, String hostname) {
+        this.name = name;
+        this.user_name = user_name;
+        this.passwd = passwd;
+        this.hostname = hostname;
+    }
+
+    public void save(String filePath) {
+        FileOutputStream fos = null;
+        ObjectOutputStream os = null;
+        try {
+
+            fos = new FileOutputStream(new File(filePath+"/"+name));
+            //fos = context.openFileOutput(filePath+"/"+name, context.MODE_PRIVATE);
+            os = new ObjectOutputStream(fos);
+            os.writeObject(this);
+            os.close();
+            fos.close();
+        }
+        catch(Exception e){Log.d("Saving error:", e.getMessage());}
+
+    }
+
+    public static Server load(String filePathPlusName) {
+        Server server = null;
+        try {
+            FileInputStream fis = new FileInputStream(new File(filePathPlusName));
+            ObjectInputStream is = new ObjectInputStream(fis);
+            server = (Server) is.readObject();
+            is.close();
+            fis.close();
+        }
+        catch(Exception e){Log.d("Loading error:", e.getMessage());}
+        return server;
+    }
+
+    public String toString() {
+        return "Server: " + this.name + ", user: " + this.user_name + ", password: " + this.passwd + ", hostname: " + this.hostname;
+    }
+ }
